7a26654741a3148b5f89559b9add9a3e5f7c8d97
himanshu <himanshu32505@gmail.com>
2025-03-06 18:48:16 +0530

    added: user module

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { login, signUp } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-
+userRouter.route("/signup").post(signUp);
+userRouter.route("/login").post(login);
 
 export default userRouter;
